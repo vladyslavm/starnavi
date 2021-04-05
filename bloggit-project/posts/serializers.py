663f1a79db48d1e615bd96441002477713da1198
Vladyslav Makaruk <vladyslavm@gmail.com>
2021-04-05 10:31:24 +0300

    Initial commit

---
+++ bloggit-project/posts/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import Post, Vote, UpVote, DownVote
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+    score = serializers.SerializerMethodField()
+    upvotes = serializers.SerializerMethodField()
+    dnvotes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created', 'score', 'upvotes', 'dnvotes']
+
+
+    def get_upvotes(self, post):
+        return UpVote.objects.filter(post=post).count()
+
+    def get_dnvotes(self, post):
+        return DownVote.objects.filter(post=post).count()
+
+    def get_score(self, post):
+        upvotes = UpVote.objects.filter(post=post).count()
+        dnvotes = DownVote.objects.filter(post=post).count()
+        return str(upvotes-dnvotes)#Vote.objects.filter(post=post).count()
+
+
+
+    
+
+class VoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vote
+        fields = ['id']
+
+
+class UpVoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UpVote
+        fields = ['id']
+
+
+class DownVoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DownVote
+        fields = ['id']
---
+++ bloggit-project/posts/views.py
@@ -0,0 +1,136 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from rest_framework import generics, permissions, mixins, status
+from .models import Post, Vote, UpVote, DownVote
+from .serializers import PostSerializer, VoteSerializer, UpVoteSerializer, DownVoteSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)            
+            return JsonResponse({'token':str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'The username has already been taken'}, status=400)
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+
+
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This is not your post')
+
+
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        post = Post.objects.get(pk=self.kwargs['pk'])
+
+        return Vote.objects.filter(voter=user, post=post)
+
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            raise ValidationError('No votes here')
+
+
+class UpVoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    
+    serializer_class = UpVoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        post = Post.objects.get(pk=self.kwargs['pk'])
+
+        return UpVote.objects.filter(voter=user, post=post)
+
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            raise ValidationError('No votes here')
+
+
+
+class DownVoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    
+    serializer_class = DownVoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        post = Post.objects.get(pk=self.kwargs['pk'])
+
+        return DownVote.objects.filter(voter=user, post=post)
+
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            raise ValidationError('No votes here')
